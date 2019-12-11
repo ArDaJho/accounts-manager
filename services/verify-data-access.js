4b3454ad7231e85094dadc51939f7e838c2503a6
David Granado Jordan <David.Granado@jalasoft.com>
2019-12-11 09:12:14 -0400

    Fix access to DATA_FOLDER_PATH variable

--- services/verify-data-access.js
+++ services/verify-data-access.js
@@ -5,12 +5,15 @@ const buildDataAccess = () => {
 
   let opsys = process.platform;
   let cpCommand = 'cp';
+  let permissionsCommand = `chmod 777 -R ${utils.DATA_FOLDER_PATH}`;
+
   if (opsys == "darwin") {
       opsys = "MacOS";
       cpCommand = 'cp';
   } else if (opsys == "win32" || opsys == "win64") {
       opsys = "Windows";
       cpCommand = 'copy';
+      permissionsCommand = 'ls';
   } else if (opsys == "linux") {
       opsys = "Linux";
       cpCommand = 'cp';
@@ -18,6 +21,7 @@ const buildDataAccess = () => {
   if (shell.exec(`cd ${utils.DATA_FOLDER_PATH}`, {silent:true}).code !== 0) {
     shell.exec(`mkdir ${utils.DATA_FOLDER_PATH}`, {silent:true});  
     shell.exec(`${cpCommand}  ${utils.DATA_PATH_PROD} ${utils.DATA_FOLDER_PATH}`, {silent:true});
+    shell.exec(permissionsCommand, {silent:true});
   }
 }
 
