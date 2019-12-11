a1e5214aff4b380e9cd5e4719387c7edc4344ad7
David Granado Jordan <David.Granado@jalasoft.com>
2019-12-11 08:22:58 -0400

    Fix access data issue

--- services/verify-data-access.js
+++ services/verify-data-access.js
@@ -2,9 +2,22 @@ const shell = require('shelljs');
 const utils = require('../utils/utils');
 
 const buildDataAccess = () => {
+
+  let opsys = process.platform;
+  let cpCommand = 'cp';
+  if (opsys == "darwin") {
+      opsys = "MacOS";
+      cpCommand = 'cp';
+  } else if (opsys == "win32" || opsys == "win64") {
+      opsys = "Windows";
+      cpCommand = 'copy';
+  } else if (opsys == "linux") {
+      opsys = "Linux";
+      cpCommand = 'cp';
+  }
   if (shell.exec(`cd ${utils.DATA_FOLDER_PATH}`, {silent:true}).code !== 0) {
-    shell.exec(`mkdir ${utils.DATA_FOLDER_PATH}`, {silent:true});
-    shell.exec(`cp  ${utils.DATA_PATH_PROD} ${utils.DATA_FOLDER_PATH}`, {silent:true});
+    shell.exec(`mkdir ${utils.DATA_FOLDER_PATH}`, {silent:true});  
+    shell.exec(`${cpCommand}  ${utils.DATA_PATH_PROD} ${utils.DATA_FOLDER_PATH}`, {silent:true});
   }
 }
 
