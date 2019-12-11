eb9543443136e3e5d5975e6cb5382889d81e1b49
David Granado Jordan <davidgjordancbba@gmail.com>
2019-12-10 22:45:14 -0400

    Add silent parameter to the shell output

--- services/verify-data-access.js
+++ services/verify-data-access.js
@@ -2,14 +2,14 @@ const shell = require('shelljs');
 const utils = require('../utils/utils');
 
 const buildDataAccess = () => {
-  if (shell.exec(`cd ${utils.DATA_FOLDER_PATH}`).code !== 0) {
-    shell.exec(`mkdir ${utils.DATA_FOLDER_PATH}`);
-    shell.exec(`cp  ${utils.DATA_PATH_PROD} ${utils.DATA_FOLDER_PATH}`);
+  if (shell.exec(`cd ${utils.DATA_FOLDER_PATH}`, {silent:true}).code !== 0) {
+    shell.exec(`mkdir ${utils.DATA_FOLDER_PATH}`, {silent:true});
+    shell.exec(`cp  ${utils.DATA_PATH_PROD} ${utils.DATA_FOLDER_PATH}`, {silent:true});
   }
 }
 
 const verifyDataAccess = () => {
-  if (shell.exec(`cd ${utils.DATA_FOLDER_PATH}`).code !== 0) return false;
+  if (shell.exec(`cd ${utils.DATA_FOLDER_PATH}`, {silent:true}).code !== 0) return false;
   return true;
 }
 
